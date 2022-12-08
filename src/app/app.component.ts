ef4123e082a927087c5722bd8eebbee82dba3bad
Shubham P <shubham.prajapat@sourcefuse.com>
2022-12-09 00:32:32 +0530

    style: add commits card (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 
+interface CommitResponse{
+  commit:{
+    message: string,
+  },
+  author: {
+    avatar_url: string,
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  JSON=JSON;
+  commits: CommitResponse[] = [];
   title = 'commits-explorer';
   // /https://api.github.com/repos/sourcefuse/loopback4-microservice-catalog/commits
+  fetchCommits(){
+    fetch(`https://api.github.com/repos/sourcefuse/loopback4-microservice-catalog/commits?per_page=100`).then(e => e.json()).then((commits: CommitResponse[]) => {
+      this.commits = commits;
+    })
+  }
 }
